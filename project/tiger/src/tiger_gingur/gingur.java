623ce74ab62ca6179e40d159c68fbda4df29b926
Delfa Fernandes <fernandesdelfa1234@gmail.com>
2024-02-11 20:40:08 +0530

    lob

--- project/tiger/src/tiger_gingur/gingur.java
+++ project/tiger/src/tiger_gingur/gingur.java
@@ -5,6 +5,16 @@ public class gingur {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a,b,c=0;
+		a=9;
+		b=9;
+		c=a+b;
+	System.out.println(" sum is :"+ c);
 	}
-
+void display()
+{
+	//c=a-b;
+	System.out.println(" sub is :");
+     
+}
 }
