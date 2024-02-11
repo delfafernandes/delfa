971e713eb76f19dcdb25909a5959a4f2789a22bc
Delfa Fernandes <fernandesdelfa1234@gmail.com>
2024-02-11 21:35:29 +0530

    lko

---
+++ pro1/page/src/page2/uoy.java
@@ -0,0 +1,15 @@
+package page2;
+
+public class uoy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a,b,c;
+		a=6;
+		b=7;
+		c=a-b;
+		System.out.println(c);
+	}
+
+}
